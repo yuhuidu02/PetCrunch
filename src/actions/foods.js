a840ceeac375e24a6f55402a9d92cca1c65d9b59
yuhuidu02 <yuhuidu02@gmail.com>
2024-06-12 12:44:58 -0400

    Add food action creators

---
+++ src/actions/foods.js
@@ -0,0 +1,48 @@
+import * as api from "../api";
+
+import {
+    FETCH_ALL_FOODS,
+    CREATE_FOOD,
+    UPDATE_FOOD,
+    DELETE_FOOD,
+} from "../constants/actionTypes";
+
+export const getFoods = () => async (dispatch) => {
+    try {
+        const { data } = await api.fetchFoods();
+        dispatch({ type: FETCH_ALL_FOODS, payload: data });
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export const createFood = (food) => async (dispatch) => {
+    console.log('food', food)
+    try {
+        const { data } = await api.createFood(food);
+        dispatch({ type: CREATE_FOOD, payload: data });
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export const updateFood = (id, food) => async (dispatch) => {
+    try {
+        const { data } = await api.updateFood(id, food);
+        dispatch({ type: UPDATE_FOOD, payload: data });
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export const deleteFood = (id) => async (dispatch) => {
+    try {
+        await api.deleteFood(id);
+        dispatch({ type: DELETE_FOOD, payload: id });
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+
+    
